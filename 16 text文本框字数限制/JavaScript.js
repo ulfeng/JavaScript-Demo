e611678ddf8ef104372c48b7f22e0daa00ec82f8
sunshine9 <1104506375@qq.com>
2015-09-10 16:21:41 +0800

    xx

---
+++ 16 text文本框字数限制/JavaScript.js
@@ -0,0 +1,15 @@
+﻿
+function checkLength() {
+    var maxChars = 20;
+    var text = document.getElementById("textArea").value;
+    if (text.length > maxChars) {
+        alert("您输入的字数超出了限制！");
+        text = text.substring(0, maxChars);
+        return false;
+    }
+    else {
+        var cur = maxChars - text.length;
+        document.getElementById("sy").innerHTML = cur.toString();
+        return true;
+    }
+}
