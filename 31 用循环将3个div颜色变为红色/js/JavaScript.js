35d1f61054b4331ccfc1f51f18f96345364051a8
sunshine9 <1104506375@qq.com>
2015-09-14 17:05:51 +0800

    鼠标移入移出改变样式

---
+++ 31 用循环将3个div颜色变为红色/js/JavaScript.js
@@ -0,0 +1,14 @@
+﻿// 方法1
+
+window.onload = function () {
+    var oBtn = document.getElementsByTagName("button")[0];
+
+    var oLi = document.getElementsByTagName("li");
+    oBtn.onclick = function () {
+        for (var i = 0; i < oLi.length; i++) {
+            oLi[i].style.backgroundColor = "red";
+        }
+    }
+}
+
+// 方法2
---
+++ 32 鼠标移入移出改变样式/js/JavaScript.js
@@ -0,0 +1,14 @@
+﻿// 方法1
+
+window.onload = function () {
+    var oDiv = document.getElementById("container");
+
+    oDiv.onmouseover = function () {
+        oDiv.style.cssText = "width: 180px;heigth: 180px;background: #fff; color: #0ff;font: 12px; margin: 0 auto; padding: 20px; border:5px solid #0ff;cursor: crosshair;";
+    }
+    oDiv.onmouseout = function () {
+        oDiv.style.cssText = "width: 180px;heigth: 180px;background: #000; color: #fff;font: 12px; margin: 0 auto; padding: 20px;";
+    }
+}
+
+// 方法2
