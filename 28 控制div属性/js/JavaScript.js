e3be97b2f405c222ac360c6b227f819efa1a1a74
sunshine9 <1104506375@qq.com>
2015-09-14 15:41:14 +0800

    网页换肤

--- 28 控制div属性/js/JavaScript.js
+++ 28 控制div属性/js/JavaScript.js
@@ -31,12 +31,13 @@ window.onload = function () {
     var oVal = ["200px", "200px", "red", "none", "block"];
 
     for (var i = 0; i < oBtn.length; i++) {
-        oBtn.index = i;
+        oBtn[i].index = i;
         oBtn[i].onclick = function () {
             this.index == oBtn.length - 1 && (oDiv.style.cssText = "");
             changeStyle(oDiv, oAtt[this.index], oVal[this.index]);
         }
     }
 }
+
 
 
--- 29 网页换肤/js/JavaScript.js
+++ 29 网页换肤/js/JavaScript.js
@@ -1,16 +1,19 @@
 ﻿// 方法1
 window.onload = function () {
+    var oLink = document.getElementById("changeStyle");
     var oSkin = document.getElementById("skin").getElementsByTagName("li");
 
     for (var i = 0; i < oSkin.length; i++) {
-        oSkin.onclick = function () {
+        oSkin[i].onclick = function () {
             for (var p in oSkin) {
                 oSkin[p].className = "";
                 this.className = "current";
+                oLink.href = "css/" + this.id + ".css";
             }
         }
     }
 }
+
 
 // 方法2
   
