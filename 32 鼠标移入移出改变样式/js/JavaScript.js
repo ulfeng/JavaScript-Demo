272c04d963f44818611d543eac6046f7907a8827
sunshine9 <1104506375@qq.com>
2015-09-14 18:53:36 +0800

    xx

--- 32 鼠标移入移出改变样式/js/JavaScript.js
+++ 32 鼠标移入移出改变样式/js/JavaScript.js
@@ -12,3 +12,12 @@ window.onload = function () {
 }
 
 // 方法2
+//window.onload = function () {
+//    var oDiv = document.getElementById("container");
+//    oDiv.onmouseover = function () {
+//        oDiv.className = "hover";
+//    };
+//    oDiv.onmouseout = function () {
+//        oDiv.className = "";
+//    }
+//}
---
+++ 33 记住密码提示框/js/JavaScript.js
@@ -0,0 +1,14 @@
+﻿// 方法1
+
+
+
+// 方法2
+//window.onload = function () {
+//    var oDiv = document.getElementById("container");
+//    oDiv.onmouseover = function () {
+//        oDiv.className = "hover";
+//    };
+//    oDiv.onmouseout = function () {
+//        oDiv.className = "";
+//    }
+//}
