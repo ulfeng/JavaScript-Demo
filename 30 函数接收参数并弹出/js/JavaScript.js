d64cc24073254f31337bec1554cffab97f9cdb37
sunshine9 <1104506375@qq.com>
2015-09-14 16:22:11 +0800

    函数接收参数并弹出

---
+++ 30 函数接收参数并弹出/js/JavaScript.js
@@ -0,0 +1,27 @@
+﻿// 方法1
+//var inputValue = document.getElementsByTagName("input");
+//var commitBtn = document.getElementsByTagName("button")[0];
+//window.onload = function () {
+//    getParameters();
+//}
+
+//function getParameters() {
+//    commitBtn.onclick = function () {
+//        alert(inputValue[0].value);
+//        alert(inputValue[1].value);
+//    }
+//}
+
+// 方法2
+var myFun = function (a, b) {
+    alert(a.value);
+    alert(b.value);
+}
+window.onload = function () {
+    var oInput = document.getElementsByTagName("input");
+    var oBtn = document.getElementsByTagName("button")[0];
+    oBtn.onclick = function () {
+        myFun(oInput[0], oInput[1]);
+    }
+}
+  
