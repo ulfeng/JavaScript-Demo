6f94dc1bec53feed6af0b594fbb74c699c005293
sunshine9 <1104506375@qq.com>
2015-09-14 10:30:55 +0800

    JavaScript 全选/反选

---
+++ 24 JavaScript 简单文本提示框/JavaScript.js
@@ -0,0 +1,10 @@
+﻿// 调用代码
+// msgText.init(text,timer)
+// text:提示框显示文本，类型String
+// timer:提示框显示时间，不设置为永久显示 单位为毫秒ms
+
+var msgText = {
+    init: function (text, timer) {
+        var _cssText="position: fixed;top: 40%;left: 45%;margin: 0 auto;background:rgba(0,0,0,0.63);color: #fff;padding: 2px 15px; "
+    }
+}
---
+++ 26 JavaScript 全选 反选/JavaScript.js
@@ -0,0 +1,56 @@
+﻿function gehang() {
+    // 获取tr节点
+    var tr = document.getElementsByTagName("tr");
+    // 为第一行添加背景颜色
+    tr[0].style.background = "#00FF66";
+    // 为最后一行添加背景颜色
+    tr[tr.length - 1].style.background = "#00FF66";
+}
+
+// 创建全选反选函数
+function xuan(type) {
+    // 获取name值
+    var qcheck = document.getElementsByName("check[]");
+    // 获取选的按钮
+    if (type == "qx") {
+        for (var i = 0; i < qcheck.length; i++) {
+            qcheck[i].check = true;
+        }
+    }
+
+    // 获取反选按钮
+    if (type == "fx") {
+        for (var i = 0; i < qcheck.length; i++) {
+            qcheck[i].checked = false;
+        }
+    }
+
+    // 取消全选
+    if (type == "qxx") {
+        for (var i = 0; i < qcheck.length; i++) {
+            if (qcheck[i].checked) {
+                qcheck[i].checked = false;
+            }
+            else {
+                qcheck[i].checked = true;
+            }
+            
+        }
+    }
+}
+
+function del() {
+    var input = document.getElementsByName("check[]");
+    for (var i = input.length; i >= 0; i--) {
+        if (input[i].checked == true) {
+            // 获取td节点
+            var td = input[i].parentNode;
+            // 获取tr节点
+            var tr = td.parentNode;
+            // 获取table
+            var table = tr.parentNode;
+            // 移除子节点
+            table.removeChild(tr);
+        }
+    }
+}
