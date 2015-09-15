aa7c8cb2a9663fe53b841e20f18cc457420210ea
sunshine9 <1104506375@qq.com>
2015-09-15 18:01:37 +0800

    xx

---
+++ 35 百度输入法/js/JavaScript.js
@@ -0,0 +1,23 @@
+﻿// 方法1
+window.onload = function () {
+    var check = document.getElementsByTagName("label")[0];
+    var tips = document.getElementById("tips");
+    check.onmouseover = function () {
+        tips.style.display = "block";
+    }
+    check.onmouseout = function () {
+        tips.style.display = "none";
+    }
+}
+
+
+// 方法2
+//window.onload = function () {
+//    var oDiv = document.getElementById("container");
+//    oDiv.onmouseover = function () {
+//        oDiv.className = "hover";
+//    };
+//    oDiv.onmouseout = function () {
+//        oDiv.className = "";
+//    }
+//}
