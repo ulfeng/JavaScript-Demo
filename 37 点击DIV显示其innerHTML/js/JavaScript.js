918d9a5d168670be7340263600a23c5a6fb240e6
sunshine9 <1104506375@qq.com>
2015-09-16 20:46:03 +0800

    输入数字求和

---
+++ 37 点击DIV显示其innerHTML/js/JavaScript.js
@@ -0,0 +1,21 @@
+﻿// 方法1
+window.onload = function () {
+    var oDiv = document.getElementById("container").getElementsByTagName("div");
+    
+    for (var i = 0; i < oDiv.length; i++) {
+        oDiv[i].onclick = function () {
+            alert(this.innerHTML);
+        }
+    }
+}
+
+// 方法2
+//window.onload = function () {
+//    var oDiv = document.getElementById("container");
+//    oDiv.onmouseover = function () {
+//        oDiv.className = "hover";
+//    };
+//    oDiv.onmouseout = function () {
+//        oDiv.className = "";
+//    }
+//}
---
+++ 37 输入数字求和/js/JavaScript.js
@@ -0,0 +1,31 @@
+﻿// 方法1
+window.onload = function () {
+    var sumBtn = document.getElementById("sumBtn");
+    var oInput = document.getElementsByTagName("input")[0]
+	oInput.onkeyup = function ()
+	{
+		this.value = this.value.replace(/[^(\d)|(,)]/, "")   // 将不为数字的替换为空
+	}
+    
+    sumBtn.onclick = function () {
+        var sum = 0;
+        var arr = document.getElementById("num").value.split(",");
+        this.value = this.value.replace(/[^(\d)|(,)]/,"")
+        for (var i in arr) {
+            sum += Number(arr[i]);
+        }
+        
+        alert(sum);
+    }
+}
+
+// 方法2
+//window.onload = function () {
+//    var oDiv = document.getElementById("container");
+//    oDiv.onmouseover = function () {
+//        oDiv.className = "hover";
+//    };
+//    oDiv.onmouseout = function () {
+//        oDiv.className = "";
+//    }
+//}
