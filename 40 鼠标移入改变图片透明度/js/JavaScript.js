5ef8ffc4946dbcee8f6ed3715e270509e790a537
sunshine9 <1104506375@qq.com>
2015-09-17 13:50:49 +0800

    鼠标移入，改变图片透明度

---
+++ 40 鼠标移入改变图片透明度/js/JavaScript.js
@@ -0,0 +1,15 @@
+﻿// 方法1
+window.onload = function () {
+    var oLi = document.getElementsByTagName("li");
+
+    for (var i in oLi) {
+        oLi[i].onmouseover = function () {
+            this.style.opacity = "1";
+        }
+        oLi[i].onmouseout = function () {
+            this.style.opacity = "0.4";
+        }
+    }
+    
+}
+
