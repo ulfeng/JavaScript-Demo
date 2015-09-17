37f1c0aed636c93c5ae1ff390b72736d60225091
sunshine9 <1104506375@qq.com>
2015-09-17 10:21:27 +0800

    弹出层效果

---
+++ 38 弹出层效果/js/JavaScript.js
@@ -0,0 +1,28 @@
+﻿// 方法1
+window.onload = function () {
+    var oButton = document.getElementById("openBox");
+    var oClose = document.getElementById("closeBox");
+    var overLay = document.getElementById("overLay");
+    var oDiv = document.getElementsByTagName("div")[2];
+
+    oButton.onclick = function () {
+        overLay.style.display = "block";
+        oDiv.style.display = "block";
+    }
+    oClose.onclick = function () {
+        overLay.style.display = "none";
+        oDiv.style.display = "none";
+    }
+
+}
+
+// 方法2
+//window.onload = function () {
+//    var oDiv = document.getElementById("container");
+//    oDiv.onmouseover = function () {
+//        oDiv.className = "hover";
+//    };
+//    oDiv.onmouseout = function () {
+//        oDiv.className = "";
+//    }
+//}
