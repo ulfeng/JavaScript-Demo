16c80c3a6f8cb53c7ed2b3fd784688452b744c8a
sunshine9 <1104506375@qq.com>
2015-09-21 10:39:49 +0800

    简易万年历

--- 41 简易选项卡 练习/js/JavaScript.js
+++ 41 简易选项卡 练习/js/JavaScript.js
@@ -1,6 +1,44 @@
 ﻿// 方法1
+//window.onload = function () {
+//    var oLi = document.getElementById("tab").getElementsByTagName("li");
+//    var main = document.getElementById("mc").getElementsByTagName("div");
+
+//    for (var i = 0; i < oLi.length; i++) {
+//        oLi[i].onclick = function () {
+//            change(this);
+//        }
+//    }
+
+//    function change(obj) {
+//        for (var i = 0; i < oLi.length; i++) {
+//            if (oLi[i] == obj) {
+//                oLi[i].className = "tab_selected";
+//                main[i].className = "main_selected";
+//            }
+//            else {
+//                oLi[i].className = "";
+//                main[i].className = "";
+//            }
+//        }
+//    }
+    
+//}
+
+// 方法2：
 window.onload = function () {
-    
-    
+    var oLi = document.getElementById("tab").getElementsByTagName("li");
+    var oDiv = document.getElementById("mc").getElementsByTagName("div");
+
+    for (var i = 0; i < oLi.length; i++) {
+        oLi[i].index = i;
+        oLi[i].onclick = function () {
+            for (var n = 0; n < oLi.length; n++) {
+                oLi[n].className = "";
+                oDiv[n].className = "";
+                this.className = "tab_selected";
+                oDiv[this.index].className = "main_selected";
+            }
+        }
+    }
 }
 
---
+++ 43 简易日历/js/JavaScript.js
@@ -0,0 +1,31 @@
+﻿// 方法1
+window.onload = function () {
+    var oLi = document.getElementById("calendar").getElementsByTagName("li");
+    var note = document.getElementById("note");
+    var arr = ["1 月节日元旦：1月1日至3日放假三天。",
+               "2 月节日元旦：1月1日至3日放假三天。",
+               "3 月节日元旦：1月1日至3日放假三天。",
+               "4 月节日元旦：1月1日至3日放假三天。",
+               "5 月节日元旦：1月1日至3日放假三天。",
+               "6 月节日元旦：1月1日至3日放假三天。",
+               "7 月节日元旦：1月1日至3日放假三天。",
+               "8 月节日元旦：1月1日至3日放假三天。",
+               "9 月节日元旦：1月1日至3日放假三天。",
+               "10 月节日元旦：1月1日至3日放假三天。",
+               "11 月节日元旦：1月1日至3日放假三天。",
+               "12 月节日元旦：1月1日至3日放假三天。"
+    ];
+    
+    for (var i = 0; i < oLi.length; i++) {
+        oLi[i].index = i;
+        oLi[i].onmouseover = function () {
+            for (var n = 0; n < oLi.length; n++) {
+                note.innerHTML = arr[this.index];
+            }
+        }
+    }
+    
+}
+
+// 方法2：
+
