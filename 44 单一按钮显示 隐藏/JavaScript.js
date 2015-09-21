9df3651ab44b4922bef775443d25c437755c1baa
sunshine9 <1104506375@qq.com>
2015-09-21 11:14:00 +0800

    单一按钮显示隐藏

---
+++ 44 单一按钮显示 隐藏/JavaScript.js
@@ -0,0 +1,11 @@
+﻿window.onload = function () {
+    var oH2 = document.getElementsByTagName("h2")[0];
+    var oUl = document.getElementsByTagName("ul")[0];
+    oH2.onclick = function () {
+        var style = oUl.style;
+        style.display = style.display == "none" ? "block" : "none";
+    }
+
+
+    
+}
