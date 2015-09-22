34d99d34e0bed2da4a1a6aa93cd5bc31ee123496
sunshine9 <1104506375@qq.com>
2015-09-22 11:30:52 +0800

    鼠标经过修改图片路径
    document.getElementById("demo").src="#";

---
+++ 46 鼠标经过，修改图片路径/js/JavaScript.js
@@ -0,0 +1,10 @@
+﻿window.onload = function () {
+    var oLi = document.getElementsByTagName("li");
+    var oImg=document.getElementById("changeSrc");
+    for (var i = 1; i < oLi.length; i++) {
+        oLi[i].index = i;
+        oLi[i].onmouseover = function () {
+            oImg.src = "images/small_"+this.index+".jpg";
+        }
+    }
+}
