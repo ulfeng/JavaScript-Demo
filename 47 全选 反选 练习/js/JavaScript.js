243122556df05a75d4625f3b6f9a92140e3ef9a6
sunshine9 <1104506375@qq.com>
2015-09-22 13:37:18 +0800

    全选/反选

---
+++ 47 全选 反选 练习/js/JavaScript.js
@@ -0,0 +1,25 @@
+﻿// 方法1
+window.onload = function () {
+    var aSelect = document.getElementById("allSelect");
+    var fSelcet = document.getElementById("fSelect");
+    var oInput = document.getElementsByTagName("input");
+
+    function isCheckAll() {
+        for (var i = 1, n = 0; i < oInput.length; i++) {
+            oInput[i].checked && n++;
+        }
+        oInput[0].checked = n == oInput.length - 1;
+        aSelect.innerHTML = oInput[0].checked ? "全不选" : "全选";
+    };
+
+    // 全选/全不选
+    aSelect.onclick = function () {
+        for (var i = 0; i < oInput.length; i++) {
+            oInput[i].checked = this.checked;
+        }
+        isCheckAll();
+    }
+}
+
+// 方法2：
+
