c3bc6b8c2dd71a323242ca0dd5f342bbd2f2dae8
sunshine9 <1104506375@qq.com>
2015-09-22 10:46:24 +0800

    提示框效果

--- 45 提示框效果/js/JavaScript.js
+++ 45 提示框效果/js/JavaScript.js
@@ -1,16 +1,27 @@
 ﻿// 方法1
 window.onload = function () {
     var oLi = document.getElementsByTagName("li");
+    var oA = document.getElementsByTagName("a");
+    var oImg = document.getElementsByTagName("img");
 
-    for (var i in oLi) {
-        oLi[i].onmouseover = function () {
-            this.style.cssText = "width:150px; height:150px;";
-
-        }
-        oLi[i].onmouseout = function () {
-            this.style.opacity = "0.4";
+    for (var i = 0; i < oLi.length; i++) {
+        oA[i].index = oImg[i].index = i;
+        oA[i].onmouseover = function () {
+            oLi[this.index].className = "zindex";
+            oImg[this.index].style.display = "block";
+        };
+        oA[i].onmouseout = function () {
+            oLi[this.index].className = "";
+            oImg[this.index].style.display = "none";
+        };
+        oImg[i].onmouseover = function () {
+            oLi[this.index].className = "zindex";
+            this.style.display = "block";
+        };
+        oImg[i].onmouseout = function () {
+            oLi[this.index].className = "";
+            this.style.display = "none";
         }
     }
-    
 }
 
