acfa387858fabdb51a576ed6bf009f6df3ab3903
sunshine9 <1104506375@qq.com>
2015-09-24 16:12:31 +0800

    倒计时时钟

---
+++ 55 简易时钟/JavaScript.js
@@ -0,0 +1,15 @@
+﻿
+function getTime() {
+    var oSapn = document.getElementsByTagName("span");
+    var d = new Date();
+    var hour = d.getHours();
+    var min = d.getMinutes();
+    var sec = d.getSeconds();
+    oSapn[0].innerHTML = hour;
+    oSapn[1].innerHTML = min;
+    oSapn[2].innerHTML = sec;
+    setTimeout("getTime()", 1000);
+}
+window.onload = function () {
+    getTime();
+}
---
+++ 56 倒计时时钟/JavaScript.js
@@ -0,0 +1,28 @@
+﻿window.onload = function () {
+    var oBtn = document.getElementsByTagName("button")[0];
+    var timer = null;
+
+    oBtn.onclick = function () {
+        this.className == "" ? (timer = setInterval(updateTime, 1000), updateTime()) : (clearInterval(timer));
+        this.className = this.className == '' ? "cancel" : '';
+    };
+
+    function format(a) {
+        return a.toString().replace(/^(\d)$/, '0$1');
+    }
+
+    function updateTime() {
+        var oSpan = document.getElementsByTagName("span");
+        var oRemain = oSpan[0].innerHTML.replace(/^0/, '') * 60 + parseInt(oSpan[1].innerHTML.replace(/^0/, ''));
+        if (oRemain <= 0) {
+            clearInterval(timer);
+            return;
+        }
+        oRemain--;
+        oSpan[0].innerHTML = format(parseFloat(oRemain / 60));
+        oRemain %= 60;
+        oSpan[1].innerHTML = format(parseInt(oRemain));
+
+    }
+
+}
