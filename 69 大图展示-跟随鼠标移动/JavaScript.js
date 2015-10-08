cd96d60d8584169493c38ee1601ae436b962c193
sunshine9 <1104506375@qq.com>
2015-10-08 10:34:31 +0800

    大图展示和自定义右键菜单

---
+++ 69 大图展示-跟随鼠标移动/JavaScript.js
@@ -0,0 +1,29 @@
+﻿window.onload = function () {
+    var aLi = document.getElementsByTagName("li");
+    var oBig = document.getElementById("big");
+    var oLoading = oBig.getElementsByTagName("div")[0];
+    var i = 0;
+
+    for (i = 0; i < aLi.length; i++) {
+        aLi[i].index = i;
+        // 鼠标划过，预加载图片插入容器并显示
+        aLi[i].onmouseover = function () {
+            var oImg = document.createElement("img");
+            // 图片预加载
+            var img = new Image();
+            img.src = oImg.src = aLi[this.index].getElementsByTagName("img")[0].src.replace(".jpg", "_big.jpg");
+            oBig.appendChild(oImg);
+            // 鼠标移过样式
+            this.className = "active";
+            // 显示big
+
+
+        }
+
+    }
+
+
+
+
+
+}
---
+++ 70 自定义右键菜单/JavaScript.js
@@ -0,0 +1,35 @@
+﻿window.onload = function () {
+    var oMenu = document.getElementById("menu");
+    var aLi = oMenu.getElementsByTagName("li");
+    // 加载后隐藏自定义右键菜单
+    oMenu.style.display = "none";
+    // 菜单鼠标移入移出样式
+    for (i = 0; i < aLi.length; i++) {
+        // 鼠标移入样式
+        aLi[i].onmouseover = function () {
+            this.className = "active";
+        };
+        // 鼠标移出样式
+        aLi[i].onmouseout = function () {
+            this.className = "";
+        }
+    }
+
+    // 自定义菜单
+    document.oncontextmenu = function (event) {
+        var event = event || window.event;
+        var style = oMenu.style;
+        style.display = "block";
+        style.top = event.clientY + "px";
+        style.left = event.clientX + "px";
+        return false;
+    };
+
+    // 页面点击后自定义菜单消失
+    document.onclick = function () {
+        oMenu.style.display = "none";
+    }
+
+
+
+}
