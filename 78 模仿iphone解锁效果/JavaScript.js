7748856d610309cb2f7b3e7983e8f736d41f9754
sunshine9 <1104506375@qq.com>
2015-10-12 09:46:39 +0800

    模仿iphone解锁效果

---
+++ 78 模仿iphone解锁效果/JavaScript.js
@@ -0,0 +1,22 @@
+﻿window.onload = function () {
+    var iPhone = document.getElementById("iphone");
+    var oLock = document.getElementById("lock");
+    var oBtn = document.getElementsByTagName("span")[0];
+    var disX = 0;
+    var maxL = oLock.clientWidth - oBtn.offsetWidth;
+    var oBg = document.createElement("img");
+    oBg.src = "http://fgm.cc/iphone/2.jpg";   // 预加载第二张图片
+    
+    oBtn.onmousedown = function (e) {
+        var e = e || window.event;
+        var l = e.clientX - disX;
+
+        1 < 0 && (1 = 0);
+        1 > maxL && (1 = maxL);
+        
+        oBtn.style.left = l + "px";
+
+
+    }
+
+}
