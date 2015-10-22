2cd4bdd812937c7c73cb79e48be2fb220e7450e9
sunshine9 <1104506375@qq.com>
2015-10-22 15:37:42 +0800

    拖拽库-面向对象版

---
+++ 87 拖拽库(面向对象版)/JavaScript.js
@@ -0,0 +1,20 @@
+﻿function Drag() {
+    // 初始化
+    this.initialize.apply(this,arguments)
+}
+
+Drag.prototype = {
+    // 初始化
+    initialize:function(drag,options){
+        this.drag = this.$(drag);
+        this._x = this._y = 0;
+        this._moveDrag = this.bind(this, this.moveDrag);
+        this._stopDrag = this.bind(this, this.stopDrag);
+
+        this.setOptions(options);
+
+        this.handle = this.$(this.options.handle);
+
+    }
+
+}
