d7a4d0b6a4b22315076ff1d101d9dcb261b69cc1
Sun Lifeng <1104506375@qq.com>
2016-01-22 11:42:30 +0800

    select 标签/图标跟着鼠标移动

---
+++ 94 js(面向对象) 模拟select标签/web/js/public.js
@@ -0,0 +1,86 @@
+﻿$(function() {
+
+    //美化select的统一调用
+    if ($(".select").length != 0) {
+        $(".select").each(function() {
+            new SameSelect($(this));
+        });
+    }
+
+});
+
+//select 替换类
+function SameSelect(obj) {
+    this.obj = obj;
+    this.opts = $("option", obj);
+    this.top = $(".top", obj);
+    this.btn = $(".btn", obj);
+    this.lis = $("li", obj);
+    this.load();
+}
+
+SameSelect.prototype = {
+    constructor: this,
+    load: function() {
+        var _this = this;
+
+        this.btn.click(function() {
+            if (_this.obj.hasClass("select_hover")) {
+                _this.hide();
+            } else {
+                _this.show();
+            }
+        });
+        this.top.click(function() {
+            if (_this.obj.hasClass("select_hover")) {
+                _this.hide();
+            } else {
+                _this.show();
+            }
+        });
+        this.lis.mouseover(function() {
+            _this.lis.removeClass("hover");
+            $(this).addClass("hover");
+        });
+        this.lis.each(function(num) {
+            $(this).click(function() {
+                _this.set(num);
+            });
+        });
+        this.obj.mouseout(function() {
+            _this.wait = setTimeout(function() {
+                _this.hide();
+            }, 2000000);
+        });
+        $("*", this.obj).mouseover(function() {
+            if (!!_this.wait) {
+                clearTimeout(_this.wait);
+            }
+        });
+    },
+    show: function() {
+        var _this = this;
+        //和top相同的li隐藏一下
+        this.lis.show();
+        this.lis.each(function() {
+            if ($(this).html() == _this.top.html()) {
+                $(this).hide();
+            }
+        });
+
+        this.obj.addClass("select_hover");
+    },
+    hide: function() {
+        this.obj.removeClass("select_hover");
+    },
+    set: function(num) {
+        var _this = this;
+        this.hide();
+        this.top.html($(this.lis[num]).html());
+        this.opts.removeAttr("selected");
+        $(this.opts[num]).attr("selected", "selected");
+    }
+};
+
+window.SameSelect = SameSelect;
+//select 替换类
---
+++ 95 图标跟着鼠标移动/web/js/luckDraw.js
@@ -0,0 +1,229 @@
+﻿$(function () {
+    // 活动时间字体弯曲
+    $(".activity_date").arctext({ radius: 2500 });
+
+    // 锤子
+    var eggModel = function (e) {
+        var n = $("#hammer"),
+            l = $(".hit_egg").offset().left,
+            d = $(".hit_egg").offset().top,
+            a,
+            t = $(".hit_egg .egg"),
+            i = -1,
+            r = -1,
+            o = !1;
+        s = $.map(t, function (e) {
+            return {
+                left: $(e).offset().left,
+                top: $(e).offset().top,
+                width: $(e).width(),
+                height: $(e).height()
+            }
+        }),
+        c = function (e, a) {
+            for (var t = 0, n = s.length; n > t; t++) {
+                var i = s[t];
+                if (i.left < e && e < i.left + i.width && i.top < a && a < i.top + i.height) return t
+            }
+            return -1
+        };
+
+        $(".hit_egg").mousemove(function (e) {
+            r = c(e.pageX, e.pageY),
+            i != r && (a && clearTimeout(a), a = setTimeout(function () {
+                -1 == r ? (t.removeClass("hover").removeClass("unhover"), n.hide()) : t.eq(r).addClass("hover").removeClass("unhover").siblings().removeClass("hover").addClass("unhover")
+            },
+            50), i = r),
+            n.css({
+                left: e.pageX - l,
+                top: e.pageY - d,
+             });
+        }).mouseleave(function () {
+            n.hide()
+        }).mouseenter(function () {
+            n.show()
+        });
+       
+    }();
+
+    // 鸡蛋/银弹/金蛋 可点击次数
+    var normalCount = 30,
+        silverCount = 10,
+        goldCount = 1;
+
+    // 鸡蛋点击事件
+    $(".hit_egg .normal").click(function () {
+        $("#hammer").css("display","none");
+        if (normalCount > 0) {
+            $(".normal").addClass("pt-normal-hit");
+            $(".normal .img_1").addClass("pt-hammer");
+            $(".normal .img_1").removeClass("hide");
+            $(".normal .img_1").css("display", "block");
+
+            setTimeout(function () {
+                $(".normal .prize").addClass("show").removeClass("hide");
+                $(".normal .hit_count").addClass("show").removeClass("hide");
+                $(".hit_egg .normal").addClass("normal_hit");
+            }, 500);
+
+            setTimeout(function () {
+                $(".hit_egg .normal").addClass("normal_hit_light");
+            }, 1700)
+            setTimeout(function () {
+                $(".hit_egg .normal").removeClass("normal_hit_light");
+            }, 1800)
+            setTimeout(function () {
+                $(".hit_egg .normal").addClass("normal_hit_light");
+            }, 1900)
+            setTimeout(function () {
+                $(".hit_egg .normal").removeClass("normal_hit_light");
+                $(".hit_egg .normal").addClass("normal_hit_crackle");
+                $(".normal .prize").addClass("hide").removeClass("show");
+                $(".normal .hit_count").addClass("hide").removeClass("show");
+            }, 2000);
+
+            setTimeout(function () {
+                $(".normal").removeClass("pt-normal-hit");
+                $(".normal").removeClass("normal_hit");
+                $(".normal").removeClass("normal_hit_crackle");
+                $(".normal .img_1").removeClass("pt-hammer");
+                $(".normal .img_1").css("display", "none");
+                $(".hit_egg .normal").addClass("normal_hit_over");
+                $(".lottery").css("display", "block");
+                $(".lottery").click(function () {
+                    $(".lottery").css("display", "none");
+                    $(".hit_egg .normal").removeClass("normal_hit_over");
+                });
+
+                normalCount -= 1;
+                $("#normal_count").text(normalCount);
+
+                $(".normal .prize").removeClass("hide");
+                $(".normal .hit_count").removeClass("hide");
+            }, 2500)
+        }
+        else {
+            $(".tip_box").css("display", "block");
+            $(".tip_box").click(function () {
+                $(".tip_box").css("display", "none");
+            })
+        };
+    })
+
+    // 银蛋点击事件
+    $(".silver").click(function () {
+        $("#hammer").css("display", "none");
+        if (silverCount > 0) {
+            $(".silver").addClass("pt-silver-hit");
+            $(".silver .img_1").addClass("pt-hammer");
+            $(".silver .img_1").removeClass("hide");
+            $(".silver .img_1").css("display", "block");
+
+            setTimeout(function () {
+                $(".silver .prize").addClass("show").removeClass("hide");
+                $(".silver .hit_count").addClass("show").removeClass("hide");
+                $(".hit_egg .silver").addClass("silver_hit");
+            }, 500)
+
+            setTimeout(function () {
+                $(".hit_egg .silver").addClass("silver_hit_light");
+            }, 1700)
+            setTimeout(function () {
+                $(".hit_egg .silver").removeClass("silver_hit_light");
+            }, 1800)
+            setTimeout(function () {
+                $(".hit_egg .silver").addClass("silver_hit_light");
+            }, 1900)
+            setTimeout(function () {
+                $(".hit_egg .silver").removeClass("silver_hit_light");
+                $(".hit_egg .silver").addClass("silver_hit_crackle");
+                $(".silver .prize").addClass("hide").removeClass("show");
+                $(".silver .hit_count").addClass("hide").removeClass("show");
+            }, 2000);
+
+            setTimeout(function () {
+                $(".silver").removeClass("pt-silver-hit");
+                $(".silver").removeClass("silver_hit");
+                $(".silver").removeClass("silver_hit_crackle");
+                $(".silver .img_1").removeClass("pt-hammer");
+                $(".silver .img_1").css("display", "none");
+                $(".hit_egg .silver").addClass("silver_hit_over");
+                $(".lottery").css("display", "block");
+                $(".lottery").click(function () {
+                    $(".lottery").css("display", "none");
+                    $(".hit_egg .silver").removeClass("silver_hit_over")
+                });
+                silverCount -= 1;
+                $("#silver_count").text(silverCount);
+                $(".silver .prize").removeClass("hide");
+                $(".silver .hit_count").removeClass("hide");
+            }, 2500)
+        }
+        else {
+            $(".tip_box").css("display", "block");
+            $(".tip_box").click(function () {
+                $(".tip_box").css("display", "none");
+            })
+        };
+    })
+
+    // 金蛋点击事件
+    $(".gold").click(function () {
+        $("#hammer").css("display", "none");
+        if (goldCount > 0) {
+            $(".gold").addClass("pt-gold-hit");
+            $(".gold .img_1").addClass("pt-hammer");
+            $(".gold .img_1").removeClass("hide");
+            $(".gold .img_1").css("display", "block");
+
+            setTimeout(function () {
+                $(".gold .prize").addClass("show").removeClass("hide");
+                $(".gold .hit_count").addClass("show").removeClass("hide");
+                $(".hit_egg .gold").addClass("gold_hit");
+            }, 500)
+
+            setTimeout(function () {
+                $(".hit_egg .gold").addClass("gold_hit_light");
+            }, 1700)
+            setTimeout(function () {
+                $(".hit_egg .gold").removeClass("gold_hit_light");
+            }, 1800)
+            setTimeout(function () {
+                $(".hit_egg .gold").addClass("gold_hit_light");
+            }, 1900)
+            setTimeout(function () {
+                $(".hit_egg .gold").removeClass("gold_hit_light");
+                $(".hit_egg .gold").addClass("gold_hit_crackle");
+                $(".gold .prize").addClass("hide").removeClass("show");
+                $(".gold .hit_count").addClass("hide").removeClass("show");
+            }, 2000);
+
+            setTimeout(function () {
+                $(".gold").removeClass("pt-gold-hit");
+                $(".gold").removeClass("gold_hit");
+                $(".gold").removeClass("gold_hit_crackle");
+                $(".gold .img_1").removeClass("pt-hammer");
+                $(".gold .img_1").css("display", "none");
+                $(".hit_egg .gold").addClass("gold_hit_over")
+                $(".lottery").css("display", "block");
+                $(".lottery").click(function () {
+                    $(".lottery").css("display", "none");
+                    $(".hit_egg .gold").removeClass("gold_hit_over")
+                });
+
+                goldCount -= 1;
+                $("#gold_count").text(goldCount);
+
+                $(".gold .prize").removeClass("hide");
+                $(".gold .hit_count").removeClass("hide");
+            }, 2500)
+        }
+        else {
+            $(".tip_box").css("display", "block");
+            $(".tip_box").click(function () {
+                $(".tip_box").css("display", "none");
+            })
+        };
+    })
+
+});
